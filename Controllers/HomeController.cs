c55b55dea511d0d06804b9949ae8a762d399e8df
Mobina Pezhman <103625277+mobinapezhman@users.noreply.github.com>
2023-09-08 17:41:28 +0330

    Upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+namespace WebApplication1.Controllers
+{
+    public class MyValue
+    {
+        public string? Title { get; set; }
+        public long Value { get; set; }
+    }
+
+
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private RepositoryDW repository;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            repository = new RepositoryDW();
+        }
+
+        public IActionResult Index()
+        {
+            var model = repository.GetDataModel();
+            return View(model);
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        
+        
+    }
+}
---
+++ Models/DashboardDataModel.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace WebApplication1.Models
+{
+    public class DashboardDataModel
+    {
+
+        public int CompanyId { get; set; }
+        public string? CompanyTitle { get; set; }
+        public int ParentId { get; set; }
+        public string? UnitTitle { get; set; }
+        public int UnitParentId { get; set; }
+        public int KpiGroupId { get; set; }
+        public string? KpiGroupTitle { get; set; }
+        public string? DimDatePersianDate { get; set; }
+        public long KpiGroupScore { get; set; }
+
+
+    }
+}
